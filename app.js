86bfa7f41bfb011ac48952e15952a20437d53291
Tariq Yahya <tariq.yahya300@gmail.com>
2021-04-02 13:38:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var width = screen.width;
+
+if (width <= 400){
+    document.getElementsByClassName('searchbar-bar')[0].placeholder = "Enter Restaurant...";
+    document.getElementById("hungry").innerHTML = "Hungry? <br> Order Now!";
+
+}
